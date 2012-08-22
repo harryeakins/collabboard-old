5b195f25c61698fad3b261f100676df5a50cf668
Harry Eakins <harry.eakins@googlemail.com>
2012-08-22 21:01:09 +0100

    Version controlling commit hooks and added install script

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-	echo "Usage: $0 test|deploy"
+	echo "Usage: $0 test|deploy|install_hooks"
 elif [ "$1" = "test" ]; then
 	echo "Running server tests"
 	node server/testSyncer.js
 elif [ "$1" = "deploy" ]; then
 	echo "Deploying"
 	echo "Not implemented"
+elif [ "$1" = "install_hooks" ]; then
+	echo "Installing hooks..."
+	ln -s .commit_hooks/pre-commit .git/hooks/pre-commit
+	echo "Finished!"
 fi
